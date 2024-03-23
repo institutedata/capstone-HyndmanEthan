a4845beac5edea8a75dc25e3da315cece3bb77f6
bally <bally.lomibao@howickltd.com>
2024-03-23 19:31:03 +1300

    Add Card model definition

---
+++ backend/models/card.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose')
+
+const cardSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    stamp: [{ type: Schema.Types.ObjectId, ref: 'Stamp' }],
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    vendor: { type: Schema.Types.ObjectId, ref: 'Vendor' },
+    // set default value to true whenever free coffee is availed
+    isArchived: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+    collection: 'Card',
+  }
+)
+
+module.exports = model('Card', cardSchema)
