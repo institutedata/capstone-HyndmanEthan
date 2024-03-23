44d4b51b0b5a147250090cdc611640878b726aa5
bally <bally.lomibao@howickltd.com>
2024-03-23 19:31:14 +1300

    Refactor pin model schema

--- backend/models/pin.js
+++ backend/models/pin.js
@@ -1,20 +1,25 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
 
-const pinSchema = new Schema({
-  vendorName: {type: String, required: true},
-  emailId: { type: String, required: true, unique: true },
-  logo: { type: String, trim: true, required: true },
-  content: {type: String, },
-  lat:{
-    type: Number,
-    required: true
+const Schema = mongoose.Schema
+
+const pinSchema = new Schema(
+  {
+    vendorName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    logo: { type: String, trim: true, required: true },
+    content: { type: String },
+    lat: {
+      type: Number,
+      required: true,
+    },
+    lng: {
+      type: Number,
+      required: true,
+    },
   },
-  lng:{
-    type: Number,
-    required: true
+  {
+    timestamps: true,
+    collection: 'Pin',
   }
-},
-{timestamps: true}
-);
-module.exports = mongoose.model("pin", pinSchema);
+)
+module.exports = mongoose.model('Pin', pinSchema)
