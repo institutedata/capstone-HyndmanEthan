6d5676c7c6e23b675530e20a09d1789587a0b74a
bally <bally.lomibao@howickltd.com>
2024-03-23 19:31:34 +1300

    Refactor vendor model schema

--- backend/models/vendor.js
+++ backend/models/vendor.js
@@ -1,13 +1,17 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const vendorSchema = new Schema({
- cafe: { type: String, trim: true, required: true },
- logo: { type: String, trim: true, required: true },
- address: { type: String, trim: true, required: true },
- emailId: { type: String, required: true, unique: true },
- password: { type: String },
- createdAt: { type: Date, default: Date.now },
- updatedAt: { type: Date, default: Date.now }
-})
-module.exports = mongoose.model("vendor", vendorSchema)
+const vendorSchema = new Schema(
+  {
+    cafe: { type: String, trim: true, required: true },
+    logo: { type: String, trim: true, required: true },
+    address: { type: String, trim: true, required: true },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true, match: [/\S+@\S+\.\S+/, 'is invalid'] },
+    password: { type: String, required: true, select: false, minLength: [6, 'Password must be at least 6 characters'] },
+  },
+  {
+    timestamps: true,
+    collection: 'Vendor',
+  }
+)
+module.exports = mongoose.model('Vendor', vendorSchema)
